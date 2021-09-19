f728c1065f96fdf2f26113e297022628c074fb7a
Fatkhulov Aynur <ainur345@yandex.ru>
2021-09-20 01:48:06 +0500

    Добавил проект с React

---
+++ Domain/Order.cs
@@ -0,0 +1,6 @@
+﻿
+namespace Domain;
+public class Order
+{
+    public int Number { get; set; }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext()
+    {
+    }
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Order> Orders { get; set; }
+}
