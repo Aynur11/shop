356215dff070dbdde35e24d2715ea4c1692c5a80
Fatkhulov Aynur <ainur345@yandex.ru>
2021-10-28 13:08:48 +0500

    Фикс ошибки

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.DTO;
 using Application.Products;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
 using System.Collections.Generic;
@@ -12,17 +13,18 @@ namespace API.Controllers
     [Route("[controller]")]
     public class ProductsController : BaseApiController
     {
-        private DataContext context;
-
-        public ProductsController(DataContext context)
+        private DataContext _context;
+        private IMapper _mapper;
+        public ProductsController(DataContext context, IMapper mapper)
         {
-            this.context = context;
+            _context = context;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<List<ProductDto>> GetProductsAsync()
         {
-            GetAllProducts.Handler handler = new GetAllProducts.Handler(context);
+            GetAllProducts.Handler handler = new GetAllProducts.Handler(_context, _mapper);
             return await handler.Handle(new GetAllProducts.Query(), new CancellationToken());
         }
     }
